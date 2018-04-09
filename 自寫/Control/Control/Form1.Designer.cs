5d82302af0b0222d81fafdcb97da71bbabbef3fb
Andrew Hua (花毓禧) <andrew.hua@pomerobotservice.com>
2018-04-09 12:12:44 +0800

    4/9全部重來版 什麼都沒有

---
+++ 自寫/Control/Control/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connect_bt = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.disconnect_bt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // connect_bt
+            // 
+            this.connect_bt.Location = new System.Drawing.Point(31, 29);
+            this.connect_bt.Name = "connect_bt";
+            this.connect_bt.Size = new System.Drawing.Size(88, 23);
+            this.connect_bt.TabIndex = 0;
+            this.connect_bt.Text = "Connect";
+            this.connect_bt.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // disconnect_bt
+            // 
+            this.disconnect_bt.Location = new System.Drawing.Point(31, 68);
+            this.disconnect_bt.Name = "disconnect_bt";
+            this.disconnect_bt.Size = new System.Drawing.Size(88, 23);
+            this.disconnect_bt.TabIndex = 2;
+            this.disconnect_bt.Text = "Disconnect";
+            this.disconnect_bt.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1199, 741);
+            this.Controls.Add(this.disconnect_bt);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.connect_bt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connect_bt;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button disconnect_bt;
+    }
+}
+
